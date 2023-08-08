5aa86692497c8302d1dc3c3a7892c7b25c4b7701
Replit user <>
2023-08-08 02:15:54 +0000

    Initial commit

--- database.py
+++ database.py
@@ -9,24 +9,27 @@ engine = create_engine(my_secret,
                         }
                       })
 
-def load_jobs_from_db():
+def load_movie_from_db():
   with engine.connect() as conn:
-    result = conn.execute(text("select * from jobs"))
-    jobs = []
+    result = conn.execute(text("select * from movie_data"))
+    movie = []
     for row in result.all():
-      jobs.append(row)
-    return jobs
+      movie.append(row)
+    return movie
 
 
 
-def load_job_from_db(id):
+
+
+
+def load_movies_from_db(id):
     with engine.connect() as conn:
-        query = text("SELECT * FROM jobs WHERE id = :job_id")
-        result = conn.execute(query.bindparams(job_id=id))
+        query = text("SELECT * FROM movie_data WHERE id = :movie_id")
+        result = conn.execute(query.bindparams(movie_id=id))
         row = result.fetchone()
         if row is not None:
-            job_dict = dict(row._asdict())
-            return job_dict
+            movie_dict = dict(row._asdict())
+            return movie_dict
         else:
             return None
 
--- main.py
+++ main.py
@@ -1,30 +1,29 @@
-from flask import Flask, jsonify, render_template, request
-from database import load_jobs_from_db, load_job_from_db 
+from flask import Flask, render_template,jsonify
+from database import load_movie_from_db,load_movies_from_db
 # add_application_to_db
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_jovian():
-  jobs = load_jobs_from_db()
-  return render_template('index.html', 
-                         jobs=jobs)
-@app.route("/job/<id>")
-def show_jobs(id):
-    job = load_job_from_db(id)
-    if job is None:
+  movie = load_movie_from_db()
+  return render_template('index.html',movie=movie)
+@app.route("/movie/<id>")
+def show_movie(id):
+    movie = load_movies_from_db(id)
+    if movie is None:
         return jsonify({})
     else:
-        return render_template("job_pages.html",jobs=job)
+        return render_template('movie.html',movie=movie)
 
-@app.route("/job/<id>/apply", methods=['post'])
-def apply_jobs(id):
-  data = request.form
-  job = load_job_from_db(id)
-  # add_application_to_db(id, data)
-  return render_template('application_submitted.html', 
-                         application=data,
-                         jobs=job)
+# @app.route("/movie/<id>/apply", methods=['post'])
+# def apply_movie(id):
+#   data = request.form
+#   movie = load_movie_from_db(id)
+#   # add_application_to_db(id, data)
+#   return render_template('application_submitted.html', 
+#                          application=data,
+#                          jobs=job)
 @app.route("/about")
 def about():
   return render_template("about.html")
