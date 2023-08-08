31692c299d69df345f1c37bbc87010fb2ffd05b5
Replit user <>
2023-08-08 03:02:54 +0000

    code modified

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from flask import Flask, render_template,jsonify
 from database import load_movie_from_db,load_movies_from_db
-# add_application_to_db
 app = Flask(__name__)
-
 
 @app.route("/")
 def hello_jovian():
@@ -16,18 +14,6 @@ def show_movie(id):
     else:
         return render_template('movie.html',movie=movie)
 
-# @app.route("/movie/<id>/apply", methods=['post'])
-# def apply_movie(id):
-#   data = request.form
-#   movie = load_movie_from_db(id)
-#   # add_application_to_db(id, data)
-#   return render_template('application_submitted.html', 
-#                          application=data,
-#                          jobs=job)
-@app.route("/about")
-def about():
-  return render_template("about.html")
-  
-
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
+
